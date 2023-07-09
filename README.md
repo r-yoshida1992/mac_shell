7fa2d6f27026fa49f6cdfac0f72084dc9366d71a
Ren Yoshida <ren.yoshida1992@gmail.com>
2023-07-09 11:31:25 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# mac_shell
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Introduction
+
+mac_shell is a collection of shell scripts for macOS that provide additional functionalities and automation for common tasks. These scripts are designed to enhance the command-line experience on macOS and simplify repetitive tasks.
+
+## Installation
+
+To use the scripts in this repository, follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```shell
+   git clone https://github.com/your-username/mac_shell.git
+
--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 SHELL_NAME=$(basename "$SHELL")
 
 if [ "$SHELL_NAME" = "bash" ]; then
-    echo 'export PATH=~/Documents/GitHub/mac_shell:$PATH' >> ~/.bash_profile
+    echo 'export PATH=~/Documents/GitHub/mac_shell/bin:$PATH' >> ~/.bash_profile
     source ~/.bash_profile
 elif [ "$SHELL_NAME" = "zsh" ]; then
-    echo 'export PATH=~/Documents/GitHub/mac_shell:$PATH' >> ~/.zshrc
+    echo 'export PATH=~/Documents/GitHub/mac_shell/bin:$PATH' >> ~/.zshrc
     source ~/.zshrc
 elif [ "$SHELL_NAME" = "fish" ]; then
-    echo 'set -x PATH ~/Documents/GitHub/mac_shell $PATH' >> ~/.config/fish/config.fish
+    echo 'set -x PATH ~/Documents/GitHub/mac_shell/bin $PATH' >> ~/.config/fish/config.fish
     source ~/.config/fish/config.fish
 else
     echo "Unsupported shell: $SHELL_NAME"
